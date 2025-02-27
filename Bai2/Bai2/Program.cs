b20826b95c99d8b87d48997da20b3873f0e12708
KhanhNguyen130804 <22115053122221@sv.ute.udn.vn>
2025-02-27 23:17:18 +0700

    up all done

--- Bai2/Bai2/Program.cs
+++ Bai2/Bai2/Program.cs
@@ -8,8 +8,19 @@ namespace Bai2
 {
     class Program
     {
+
+        // Hàm tính tổng với tham số tùy chọn (mặc định b = 10)
+        static int Sum221(int a221, int b221 = 10)
+        {
+            return a221 + b221;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Tong khi chỉ truyền một số: " + Sum221(5)); // Mặc định b = 10
+            Console.WriteLine("Tong khi truyền đủ hai số: " + Sum221(5, 20)); // b = 20
+
+            Console.ReadKey();
+
         }
     }
 }
